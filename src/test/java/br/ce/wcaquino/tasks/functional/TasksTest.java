42753bdfb3923b4ff1e991a70515b0bc40f30629
Andre Bueno <decobueno@gmail.com>
2021-10-24 12:54:23 -0300

    criando teste de remocao de tarefa

--- src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -20,9 +20,9 @@ public class TasksTest {
 		//WebDriver driver = new ChromeDriver();
 		// usando o selenium grid para execução nos nós do grid
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
-		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.4:4444/wd/hub"), cap);
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.9:4444/wd/hub"), cap);
 		//driver.navigate().to("http://localhost:8001/tasks");
-		driver.navigate().to("http://192.168.0.4:8001/tasks");
+		driver.navigate().to("http://192.168.0.9:8001/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
 		return driver;
@@ -149,6 +149,50 @@ public class TasksTest {
 			String message = driver.findElement(By.id("message")).getText();
 			
 			Assert.assertEquals("Due date must not be in past", message);
+		} finally {
+			// fechar o browser
+			//
+			driver.quit();
+		}
+	}
+	
+	@Test
+	public void shouldRemoveTask() throws MalformedURLException
+	{	
+		WebDriver driver = accessApplication();
+		
+		try {
+			// inserir uma tarefa
+			//
+			driver.findElement(By.id("addTodo")).click();
+			
+			// escrever a descrição
+			//
+			driver.findElement(By.id("task")).sendKeys("Teste via Selenium");
+			
+			// escrever a data
+			//
+			driver.findElement(By.id("dueDate")).sendKeys("10/10/2030");
+			
+			// clicar em salvar
+			//
+			driver.findElement(By.id("saveButton")).click();
+			
+			// validar mensagem de sucesso
+			//
+			String message = driver.findElement(By.id("message")).getText();
+			
+			Assert.assertEquals("Success!", message);
+			
+			// remover a tarefa
+			driver.findElement(By.xpath("//a[@class='btn btn-outline-danger btn-sm]'")).click();
+			
+			// validar mensagem de sucesso
+			//
+			message = driver.findElement(By.id("message")).getText();
+						
+			Assert.assertEquals("Success!", message);
+			
 		} finally {
 			// fechar o browser
 			//
